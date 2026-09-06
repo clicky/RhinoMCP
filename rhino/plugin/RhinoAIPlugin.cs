840719c0b54310deb80e0a96cc7984658e12a34b
Callum Sykes <28632338+clicky@users.noreply.github.com>
2026-09-06 14:24:50 -0700

    Fix missing icon on windows

--- rhino/plugin/RhinoAIPlugin.cs
+++ rhino/plugin/RhinoAIPlugin.cs
@@ -38,8 +38,17 @@ public class RhinoAIPlugin : PlugIn
         try
         {
             Assembly assembly = typeof(RhinoAIPlugin).Assembly;
-            System.Drawing.Icon icon = Rhino.UI.DrawingUtilities.IconFromResource(IconResourceName, assembly);
-            return icon;
+            using Stream? stream = assembly.GetManifestResourceStream(IconResourceName);
+            if (stream is null)
+                return null;
+
+            using StreamReader reader = new(stream);
+            string svg = reader.ReadToEnd();
+
+            Size size = Rhino.UI.Panels.IconSizeInPixels;
+            int pixels = size.Width > 0 ? size.Width : 36;
+            using Bitmap bitmap = Rhino.UI.DrawingUtilities.BitmapFromSvg(svg, pixels, pixels, adjustForDarkMode: true);
+            return System.Drawing.Icon.FromHandle(bitmap.GetHicon());
         }
         catch
         {
