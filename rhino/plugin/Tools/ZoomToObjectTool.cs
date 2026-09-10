508b93660095361572b32261c33b3b259797f910
Callum Sykes <callumsykes@protonmail.com>
2026-09-10 10:49:22 -0700

    Better ZoomToObjects guidance

--- rhino/plugin/Tools/ZoomToObjectTool.cs
+++ rhino/plugin/Tools/ZoomToObjectTool.cs
@@ -15,19 +15,37 @@ internal static class ZoomToObjectTool
         if (doc.IsHeadless)
             return Failure(ToolError.RH_Doc_Headless);
 
+        if (ids is null || ids.Length <= 0)
+            return Failure(ToolError.BadArgument, "No ids passed to tool", "Pass in a list of object IDs");
+
         BoundingBox bb = BoundingBox.Empty;
+
+        Coercions coercions = new();
 
         foreach (string idStr in ids)
         {
-            if (!Guid.TryParse(idStr, out Guid guid)) continue;
-            Rhino.DocObjects.RhinoObject obj = doc.Objects.FindId(guid);
-            if (obj?.Geometry is null) continue;
+            if (!Guid.TryParse(idStr, out Guid guid))
+            {
+                coercions.Note($"{idStr} was not valid, Only pass in valid GUIDs");
+                continue;
+            }
+
+            DocObjects.RhinoObject obj = doc.Objects.FindId(guid);
+            if (obj?.Geometry is null)
+            {
+                coercions.Note($"Object with {idStr} had no geometry, it cannot be zoomed to.");
+                continue;
+            }
+
             bb.Union(obj.Geometry.GetBoundingBox(true));
         }
 
         if (!bb.IsValid)
-            return Failure(ToolError.RH_Object_NotFound, "No valid objects found.");
+        {
+            return Failure(ToolError.RH_Object_NotFound, "No valid objects found.", coercions.Guidance);
+        }
 
+        // TODO : Create a viewport automatically?
         RhinoViewport? vp = doc.Views.ActiveView?.ActiveViewport;
         if (vp is null)
             return Failure(ToolError.RH_View_NotFound, guidance: "Ask the user to open a viewport");
